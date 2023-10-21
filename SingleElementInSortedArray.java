5a2c85a0012fa4bf841cf587196d0460545ca659
Sahil Kumar <124178990+Sahilkumar19@users.noreply.github.com>
2023-10-21 22:08:03 +0530

    Adding SingleElementInSortedArray
    Medium question of leet code but it is easy , I have done it using bitwise operator.

---
+++ SingleElementInSortedArray.java
@@ -0,0 +1,23 @@
+/*
+You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once.
+Return the single element that appears only once.
+Your solution must run in O(log n) time and O(1) space.
+
+Example 1:
+Input: nums = [1,1,2,3,3,4,4,8,8]
+Output: 2
+ */
+public class SingleElementInSortedArray {
+    public static void main(String[] args) {
+        int[] nums={1,1,2,3,3,4,4,8,8};
+        int ans=singleNonDuplicate(nums);
+        System.out.println(ans);
+    }
+    public static int singleNonDuplicate(int[] nums) {
+        int unique=0;
+        for(int n:nums){
+            unique^=n;
+        }
+        return unique;
+    }
+}
