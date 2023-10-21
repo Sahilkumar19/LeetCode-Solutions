5b1bd8c503a15a27384cd4182fdaa538f2af3339
Sahil Kumar <124178990+Sahilkumar19@users.noreply.github.com>
2023-10-21 09:55:46 +0530

    Adding SumTwo

---
+++ SumTwo.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ */
+public class SumTwo {
+    public static void main(String[] args) {
+        int[] nums={2,7,11,15};
+        int target=9;
+        int[] ans=twoSum(nums,target);
+        System.out.println(Arrays.toString(ans));
+
+    }
+    public static int[] twoSum(int[] nums, int target) {
+        int[] ans = {-1,-1};
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i]+nums[j]==target){
+                    ans[0]=i;
+                    ans[1]=j;
+
+                }
+            }
+        }
+        return ans;
+    }
+}
