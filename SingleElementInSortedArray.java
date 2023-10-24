dab2761d0b018351128f5db45024893a2617ba93
Sahil Kumar <124178990+Sahilkumar19@users.noreply.github.com>
2023-10-24 14:51:53 +0530

    Adding SingleElementInSortedArray

--- SingleElementInSortedArray.java
+++ SingleElementInSortedArray.java
@@ -3,7 +3,10 @@ You are given a sorted array consisting of only integers where every element app
 Return the single element that appears only once.
 Your solution must run in O(log n) time and O(1) space.
 
+
+
 Example 1:
+
 Input: nums = [1,1,2,3,3,4,4,8,8]
 Output: 2
  */
