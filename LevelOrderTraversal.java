e285325cee92d9a7374ea668b066741ad663b910
sahil kumar <kumarsahil7753@gmail.com>
2023-11-03 19:38:19 +0530

    Adding Question details

--- LevelOrderTraversal.java
+++ LevelOrderTraversal.java
@@ -1,5 +1,10 @@
+
 /*
-recursively find the numbers of nodes in a binary tree
+recursively find the total number of nodes in a binary tree.(asked in my university exam)
+
+Example:
+root=[1,2,3]
+output=3
  */
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -8,46 +13,49 @@ import java.util.Queue;
 
 public class LevelOrderTraversal {
     public static void main(String[] args) {
-        Node1 root=new Node1(1);
-        root.left=new Node1(2);
-        root.right=new Node1(3);
+        Node1 root = new Node1(1);
+        root.left = new Node1(2);
+        root.right = new Node1(3);
         System.out.println(BFSTraversal(root));
     }
-    public static int BFSTraversal(Node1 root){
-        List<Integer> result=new ArrayList<>();
-        if (root==null){
+
+    public static int BFSTraversal(Node1 root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
             return 0;
         }
-        Queue<Node1> queue=new LinkedList<>();
+        Queue<Node1> queue = new LinkedList<>();
         queue.offer(root);
-        while (!queue.isEmpty()){
-            int levelsize= queue.size();
+        while (!queue.isEmpty()) {
+            int levelsize = queue.size();
             for (int i = 0; i < levelsize; i++) {
-                Node1 cur=queue.poll();
-                if (cur.left!=null){
+                Node1 cur = queue.poll();
+                if (cur.left != null) {
                     queue.offer(cur.left);
                     result.add(cur.left.data);
                     BFSTraversal(cur.left);
 
                 }
-                if (cur.right!=null){
+                if (cur.right != null) {
                     queue.offer(cur.right);
                     result.add(cur.right.data);
                     BFSTraversal(cur.right);
                 }
             }
         }
-        return result.size()+1;
+        return result.size() + 1;
 
     }
 }
-//nodes of the tree
-class Node1{
+
+// nodes of the tree
+class Node1 {
     int data;
-    Node1 left,right;
-    public Node1(int key){
-        key=data;
-        left=null;
-        right=null;
+    Node1 left, right;
+
+    public Node1(int key) {
+        key = data;
+        left = null;
+        right = null;
     }
 }
